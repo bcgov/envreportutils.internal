be525be9bb9022cef5caa0eb253e2412b4098d32
Andy Teucher <andy.teucher@gmail.com>
2015-03-06 16:04:20 -0800

    Add vignette

---
+++ inst/doc/indicator.R
@@ -0,0 +1,38 @@
+## ----set-options, echo = FALSE-------------------------------------------
+knitr::opts_chunk$set(eval=FALSE)
+
+
+## ------------------------------------------------------------------------
+#  envreportbc::install_local("envreportbc")
+#  library("envreportbc")
+
+## ------------------------------------------------------------------------
+#  setwd("c:/_dev/")
+#  
+#  indicator_skeleton(path = ".", bucket = "Contaminants", git_init = FALSE,
+#                     git_clone = "D:/git_server/contaminants/tarballs")
+
+## ------------------------------------------------------------------------
+#  envreportbc::install_local("envreportbc")
+#  library("envreportbc")
+
+## ------------------------------------------------------------------------
+#  setwd("c:/_dev/")
+#  
+#  indicator_skeleton(path = ".", bucket = "Contaminants", git_init = FALSE,
+#                     git_clone = "https://github.com/bcgov/tarballs.git")
+
+## ------------------------------------------------------------------------
+#  envreportbc::install_local("envreportbc")
+#  library("envreportbc")
+
+## ------------------------------------------------------------------------
+#  setwd("c:/_dev/tarballs") ## Assuming this directory already exists)
+#  
+#  indicator_skeleton(path = ".", bucket = "Contaminants", git_init = FALSE)
+
+## ------------------------------------------------------------------------
+#  setwd("c:/_dev")
+#  
+#  indicator_skeleton(path = "tarballs", bucket = "Contaminants", git_init = FALSE)
+
