1c3e991c3a460ffed04957e64df68cf45dc3e092
Andy Teucher <andy.teucher@gov.bc.ca>
2015-07-21 14:24:52 -0700

    Fix calls to add_file_from_template

--- R/slide_template.R
+++ R/slide_template.R
@@ -10,24 +10,27 @@
 #' slide_template("my_pres", path = "presentation")
 slide_template <- function(title, path = ".") {
   if (missing(title)) stop("You must specify a title")
+  if (!dir.exists(path)) dir.create(path)
+  
   envreportutils:::add_file_from_template(path, 
-                        fname = "io_presentation/presentation_template.Rmd", 
-                        outfile = paste0(title, ".Rmd"))
+                        fname = file.path("io_presentation", "presentation_template.Rmd"), 
+                        outfile = paste0(title, ".Rmd"), pkg = "envreportbc")
   envreportutils:::add_file_from_template(file.path(path, "css"), 
-                         fname = "io_presentation/css/envreportbc-style.css", 
-                         outfile = "envreportbc-style.css")
+                         fname = file.path("io_presentation","css","envreportbc-style.css"), 
+                         outfile = "envreportbc-style.css", pkg = "envreportbc")
   
   pngs <- c("BC_MoE_logo.png", "come-in-open.png", "databc.png", 
             "EnvironmentalReportingBC_logo.png", "envreportbc-github.png", 
             "pause_sink_dataviz.png", "rising-value.png", "SoE_books.png", 
             "state-salish-sea.png", "unep-soe_gateway.png")
   
-  pngs <- file.path("io_presentation/img", pngs)
+  pngs <- file.path("io_presentation", "img", pngs)
   
   lapply(pngs, function(png) {
     envreportutils:::add_file_from_template(file.path(path, "img"), 
                                             fname = png, 
-                                            outfile = basename(png))
+                                            outfile = basename(png), 
+                                            pkg = "envreportbc")
   })
 
   invisible(TRUE)
