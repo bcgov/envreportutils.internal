a92c6ec76d4719e5f37c980f3408b9f4205fec21
Andy Teucher <andy.teucher@gov.bc.ca>
2015-08-31 15:55:24 -0700

    Change package to envreportbc

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
                           "Without it you will not be able to embed fonts in pdfs. ", 
                           "If you require this, please install ghostscript from ", 
                           "'http://ghostscript.com/download/gsdnld.html' and ",
-                          "run envreportutils:::set_ghostscript('path_to_executable')")
+                          "run envreportbc:::set_ghostscript('path_to_executable')")
   }
 }
 
@@ -12,7 +12,7 @@
 #' @param gsfile path to the ghostscript executable
 #' @keywords internal
 #' @return logical dependent on whether or not the file exists
-set_ghostscript <- function(gsfile = "C:/Program Files/gs/gs9.15/bin/gswin64c.exe") {
+set_ghostscript <- function(gsfile = "C:/Program Files/gs/gs9.16/bin/gswin64c.exe") {
   ret <- FALSE
   if (file.exists(gsfile)) {
     Sys.setenv(R_GSCMD=gsfile)
