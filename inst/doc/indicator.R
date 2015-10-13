d40b397f4aa7de0cc6663460eefe617c99fde10e
Andy Teucher <andy.teucher@gov.bc.ca>
2015-10-13 16:25:39 -0700

    Rerun vignette

--- inst/doc/indicator.R
+++ inst/doc/indicator.R
@@ -1,6 +1,5 @@
 ## ----set-options, echo = FALSE-------------------------------------------
-knitr::opts_chunk$set(eval=FALSE)
-
+knitr::opts_chunk$set(eval = FALSE)
 
 ## ------------------------------------------------------------------------
 #  envreportbc::install_dev("envreportbc")
@@ -8,27 +7,30 @@ knitr::opts_chunk$set(eval=FALSE)
 
 ## ------------------------------------------------------------------------
 #  setwd("c:/_dev/")
-#  indicator_skeleton(path = ".", bucket = "Contaminants", git_init = FALSE,
+#  indicator_skeleton(path = ".", git_init = FALSE,
 #                     git_clone = "D:/git_server/contaminants/tarballs.git")
 
 ## ------------------------------------------------------------------------
-#  envreportbc::install_local("envreportbc")
+#  envreportbc::install_dev("envreportbc")
 #  library("envreportbc")
 
 ## ------------------------------------------------------------------------
 #  setwd("c:/_dev/")
-#  indicator_skeleton(path = ".", bucket = "Contaminants", git_init = FALSE,
+#  indicator_skeleton(path = ".", git_init = FALSE,
 #                     git_clone = "https://github.com/bcgov/tarballs.git")
 
 ## ------------------------------------------------------------------------
-#  envreportbc::install_local("envreportbc")
+#  envreportbc::install_dev("envreportbc")
 #  library("envreportbc")
 
 ## ------------------------------------------------------------------------
 #  setwd("c:/_dev/tarballs") ## Assuming this directory already exists)
-#  indicator_skeleton(path = ".", bucket = "Contaminants", git_init = FALSE)
+#  indicator_skeleton(path = ".", git_init = FALSE)
 
 ## ------------------------------------------------------------------------
 #  setwd("c:/_dev")
-#  indicator_skeleton(path = "tarballs", bucket = "Contaminants", git_init = FALSE)
+#  indicator_skeleton(path = "tarballs", git_init = FALSE)
+
+## ------------------------------------------------------------------------
+#  create_print_ver(html = "p:/indicators/sustainability/tarballs.html")
 
