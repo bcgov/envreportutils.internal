186416813a63ab339e9fff6b232042e6f19ff030
Andy Teucher <andy.teucher@gov.bc.ca>
2015-10-13 15:05:14 -0700

    Do a better job of finding Ghostscript

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   if (!set_ghostscript()) {
-    packageStartupMessage("You do not appear to have ghostscript installed. ",
+    packageStartupMessage("You do not appear to have ghostscript installed in ", 
+                          "the usual place (C:/Program Files/gs/). ",
                           "Without it you will not be able to embed fonts in pdfs. ", 
                           "If you require this, please install ghostscript from ", 
                           "'http://ghostscript.com/download/gsdnld.html' and ",
@@ -12,10 +13,15 @@
 #' @param gsfile path to the ghostscript executable
 #' @keywords internal
 #' @return logical dependent on whether or not the file exists
-set_ghostscript <- function(gsfile = "C:/Program Files/gs/gs9.16/bin/gswin64c.exe") {
+set_ghostscript <- function(gsfile = NULL) {
+  if (is.null(gsfile)) {
+    gsfile <- list.files("C:/Program Files/gs", recursive = TRUE, 
+                         full.names = TRUE, pattern = "gswin(32|64)c\\.exe")
+    gsfile <- gsfile[length(gsfile)] # If more than one, get the last, should be most recent
+  }
   ret <- FALSE
   if (file.exists(gsfile)) {
-    Sys.setenv(R_GSCMD=gsfile)
+    Sys.setenv(R_GSCMD = gsfile)
     ret <- TRUE
   }
   ret
